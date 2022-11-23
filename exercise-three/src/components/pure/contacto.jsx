fe0054cca945951efbee6201a53f233a6e7a922e
ccmarin14 <cristiancamilomarinflorez@gmail.com>
2022-11-23 06:10:21 -0500

    Up exercise Three

--- exercise-three/src/components/pure/contacto.jsx
+++ exercise-three/src/components/pure/contacto.jsx
@@ -6,28 +6,29 @@ import EstadoComponent from './estado.jsx';
 const ContactoComponent = ({contacto,mostrar,borrar}) => {
 
     const [ver, setVer] = useState(mostrar);
+    const [estado, setEstado] = useState(contacto.conectado);
 
     const mostrarContacto = () => {
         if (ver) {
             return (
                 <div>
                     <div>
-                        {contacto.email}
+                        <b>Email:</b> {contacto.email}
+                    </div>              
+                    <div>
+                        <EstadoComponent conectado = {estado}></EstadoComponent>
                         <i
-                            className='bi bi-trash2' 
+                            className='bi bi-arrow-down-up' 
                             style={{
-                                color:'tomato', 
+                                color:'green',
                                 position:'absolute', 
                                 right:'0',
                                 paddingRight:'10px', 
                                 cursor:'grab'
                             }}
-                            onClick={()=>{borrar(contacto)}}
+                            onClick={()=>conmutarEstado()}
                         >
                         </i>
-                    </div>              
-                    <div>
-                        <EstadoComponent conectado = {contacto.conectado}></EstadoComponent>
                     </div>
                 </div>
             )
@@ -38,24 +39,36 @@ const ContactoComponent = ({contacto,mostrar,borrar}) => {
         setVer(!ver);
     }
 
+    const conmutarEstado = () => {
+        setEstado(!estado);
+    }
+
     return (
         <li 
             style={{listStyle:'none'}}
         >
-            <div
-                className='border p-1 rounded' 
-                style={{cursor:'pointer'}}
-                onClick={()=>conmutarMostrar()}           
-            >
-                <div>
+            <div className='border p-1 rounded' >
+                <div 
+                    className='border-bottom border-opacity-25 border-success'
+                    style={{cursor:'pointer'}}
+                    onClick={()=>conmutarMostrar()}
+                >
                     <b>{contacto.nombre} {contacto.apellido}</b>
-                    <i 
-                        className='bi bi-arrow-return-left' 
-                        style={{color:'green', position:'absolute', right:'0', paddingRight:'10px'}}
+                    <i
+                        className='bi bi-trash2' 
+                        style={{
+                            color:'tomato', 
+                            position:'absolute', 
+                            right:'0',
+                            paddingRight:'10px', 
+                            cursor:'grab'
+                        }}
+                        onClick={()=>{borrar(contacto)}}
                     >
                     </i>
-                    {mostrarContacto()}
+                    
                 </div>
+                {mostrarContacto()}
             </div>
         </li>
     );
@@ -63,6 +76,8 @@ const ContactoComponent = ({contacto,mostrar,borrar}) => {
 
 ContactoComponent.propTypes = {
     contacto: PropTypes.instanceOf(Contacto).isRequired,
+    mostrar: PropTypes.bool.isRequired,
+    borrar: PropTypes.func.isRequired,
 };
 
 export default ContactoComponent;
