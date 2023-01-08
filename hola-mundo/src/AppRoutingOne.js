2aed4132253796738cc5535c863f12ebb2321a2f
ccmarin14 <cristiancamilomarinflorez@gmail.com>
2023-01-07 21:58:18 -0500

    Update 07012023

--- hola-mundo/src/AppRoutingOne.js
+++ hola-mundo/src/AppRoutingOne.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import NotFoundPage from './pages/404/NotFoundPage'
@@ -5,16 +6,32 @@ import Aboutpage from './pages/about-faqs/Aboutpage';
 import ProfilePage from './pages/profile/ProfilePage';
 import TaskPage from './pages/tasks/TaskPage';
 import TaskDetailPage from './pages/tasks/TaskDetailPage';
+import LoginPage from './pages/auth/LoginPage';
 
-function AppRoutingOne() {
+const CustomRouteHome = ({message}) => {
+  alert(message);
+  return(<Navigate redirect to='/' />);
+}
 
-  const logged = false;
+const CustomRouteLogin = ({message}) => {
+  alert(message);
+  return(<LoginPage />);
+}
+
+async function AppRoutingOne() {
+
+  let logged = localStorage.getItem('credentials');
+
+  useEffect(() => {
+    logged =  localStorage.getItem('credentials');
+  }, []);
 
   return (
     <Router>
       <div>
         <aside>
           <Link to='/'>| HOME |</Link>
+          <Link to='/login'>| LOGIN |</Link>
           <Link to='/about'>| ABOUT |</Link>
           <Link to='/faqs'>| FAQS |</Link>
           <Link to='/any404'>| NotFound |</Link>
@@ -22,16 +39,20 @@ function AppRoutingOne() {
       </div>
       <Routes>
         <Route exact path="/" element={ <HomePage /> } />
+        <Route path="/login" element={
+          logged ?
+          <CustomRouteHome message={'You are logged in. Redirecting to home ...'} />:
+          <CustomRouteLogin message={'You must be logged in. Redirecting to login ...'} />          
+        } />
         <Route path="/about" element={ <Aboutpage /> } />
         <Route path="/faqs" element={ <Aboutpage /> } />
-        <Route path='/profile' element={ 
-          logged ? 
-          <ProfilePage />: 
-          <Navigate replace to="/" />
-        }>
-        </Route>
-        <Route path='/tasks' element={ <TaskPage /> }></Route>
-        <Route path='/task/:id' element={ <TaskDetailPage /> }></Route>
+        <Route path='/profile' element={
+          logged ?
+          <ProfilePage />:
+          <CustomRouteLogin message={'You must be logged in. Redirecting to login ...'} />
+        } />
+        <Route path='/tasks' element={ <TaskPage /> } />
+        <Route path='/task/:id' element={ <TaskDetailPage /> } />
         {/* 404 - Page No Found */}
         <Route path="/*" element={ <NotFoundPage /> } />
       </Routes>
